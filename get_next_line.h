8e8d88976dd0d89b65f04819343a41831bf104af
mohamedicho99 <mohamed.icho.99@gmail.com>
2025-01-11 11:54:17 +0100

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohel-mo <mohel-mo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 22:36:04 by mohel-mo          #+#    #+#             */
+/*   Updated: 2024/12/31 23:48:20 by mohel-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 void	write_to_fd(int fd, char *buffer, int ln);
@@ -20,5 +32,7 @@ void	ft_memcpy(char *dst, const char *src, size_t n);
 char	*reset_cache(char **cache);
 void	*ft_calloc(size_t n_el, size_t size);
 char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohel-mo <mohel-mo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 22:36:11 by mohel-mo          #+#    #+#             */
+/*   Updated: 2024/12/31 23:29:16 by mohel-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 void	write_to_fd(int fd, char *buffer, int ln)
 {
-    write(fd, buffer, ln);
+	write(fd, buffer, ln);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size) 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	l_src;
 	size_t	i;
@@ -27,14 +39,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 
 int	ft_strlen(const char *s)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    if (!s)
-        return (i);
-    while (s && s[i])
-        i++;
-    return (i);
+	i = 0;
+	if (!s)
+		return (i);
+	while (s && s[i])
+		i++;
+	return (i);
 }
 
 char	*ft_strdup(const char *s)
@@ -57,68 +69,22 @@ char	*ft_strdup(const char *s)
 	return (dst);
 }
 
-char *ft_strjoin(char *s1, char *s2)
-{
-    char *n_str;
-    size_t s1_len;
-    size_t s2_len;
-
-	if ((!s1 || s1[0] == '\0') && (!s2 || s2[0] == '\0'))
-		return (NULL);
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	n_str = malloc(s1_len + s2_len + 1 * sizeof(char));
-	if (!n_str)
-		return (NULL);
-	ft_strlcpy(n_str, s1, s1_len + 1);
-	ft_strlcpy(n_str + s1_len, s2, s2_len + 1);
-    free(s1);
-	return (n_str);
-}
-
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	unsigned char	*ptr;
 	unsigned char	cc;
 	size_t			i;
 
-	ptr = (unsigned char*)s;
+	ptr = (unsigned char *)s;
 	cc = (unsigned char)c;
 	i = 0;
 	while (ptr[i])
 	{
 		if (ptr[i] == cc)
-			return ((char*)ptr + i);
+			return ((char *)ptr + i);
 		i++;
 	}
 	if (cc == '\0')
-		return ((char*)ptr + i);
+		return ((char *)ptr + i);
 	return (NULL);
-}
-
-char	*reset_cache(char **cache)
-{
-	char	*ptr;
-	char	*line;
-	char	*tmp;
-	size_t	ptr_l;
-
-	ptr = ft_strchr(*cache, '\n');
-	if (!ptr)
-		return (free(*cache), *cache = NULL, NULL);
-	ptr++;
-	ptr_l = (ptr - *cache) + 1;
-	line = malloc(ptr_l * sizeof(char));
-	if (!line)
-		return (free(*cache), *cache = NULL, NULL);
-	ft_strlcpy(line, *cache, ptr_l);
-	tmp = ft_strdup(ptr);
-	if (!tmp)
-		return (free(line), line = NULL, free(*cache), cache = NULL, NULL);
-	free(*cache);
-	return (*cache = tmp, line);
 }
